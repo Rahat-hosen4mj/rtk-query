275d7d2052807321f8c407463be4724da3295b54
Your Name <you@example.com>
2022-11-30 20:03:08 +0600

    first time fetch data using rtk qury

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import { useGetAllPostQuery } from './services/post'
 
 function App() {
-  const {data: allPost, isLoading} = useGetAllPostQuery();
+  const {data: allPost, isLoading, isError, error} = useGetAllPostQuery();
   if(isLoading){
     return <p>loading...</p>
   }
-  console.log(allPost);
+  if(isError){
+    return <p>{error.error}</p>
+  }
+  console.log('all response from ', allPost);
   return (
     <div className="App">
-      <h2>Hello rtk query</h2>
+      <h2>allPost : {allPost.length}</h2>
+    {
+      allPost?.map((post, i) =>
+       <div key={i}>
+        <h3>{post.id}:{post.title}</h3>
+        <p>{post.body}</p>
+       </div>
+      )
+    }
     </div>
   );
 }
